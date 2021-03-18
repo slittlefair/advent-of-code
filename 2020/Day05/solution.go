b774173b2763a01672e937628a35312b55c10c25
Sam Littlefair <sam.littlefair@sparx.co.uk>
2021-03-18 10:58:59 +0000

    Tweaks and improvements

--- 2020/Day05/solution.go
+++ 2020/Day05/solution.go
@@ -6,6 +6,11 @@ import (
 	"fmt"
 )
 
+// halfSeats takes the string of directions, the min seat and the max seat and returns a new string,
+// min and max. It takes the direction as the first character in the string and returns an error if
+// it's not valid. Otherwise it partitions the seats depending on the direction and alters the min
+// or max accordingly. It returns these, as well as the remainder of the directions, minus the
+// direction we just evaluated.
 func halfSeats(pass string, min int, max int) (string, int, int, error) {
 	direction := string(pass[0])
 	if direction != "F" && direction != "B" && direction != "L" && direction != "R" {
@@ -19,6 +24,11 @@ func halfSeats(pass string, min int, max int) (string, int, int, error) {
 	return string(pass[1:]), min, max, nil
 }
 
+// findMyID takes a map of IDs we know are taken, the lowest and the highest IDs in the map. It then
+// cycles through each ID between the two limits and returns the first ID (there is only one) not in
+// the map, which is ours. We are told that the seat with the ID +1 and -1 to ours is taken, so we
+// don't have an edge seat and therefore must be in the range of lowest to highest. We can therefore
+// return an error if we can't find a "blank" ID
 func findMyID(usedIDs map[int]bool, lowestID int, highestID int) (int, error) {
 	for i := lowestID; i <= highestID; i++ {
 		if _, ok := usedIDs[i]; !ok {
@@ -30,7 +40,7 @@ func findMyID(usedIDs map[int]bool, lowestID int, highestID int) (int, error) {
 
 func part1(entries []string) (int, int, map[int]bool, error) {
 	highestID := 0
-	lowestID := 10000000
+	lowestID := (127 * 8) + 7
 	usedIDs := make(map[int]bool)
 	for _, entry := range entries {
 		minRow := 1
@@ -38,9 +48,10 @@ func part1(entries []string) (int, int, map[int]bool, error) {
 		minCol := 1
 		maxCol := 8
 		var err error
+		// Keep running halfSeats on the trimmed directions until we have partitioned to only one
+		// row/column.
 		for minRow != maxRow {
 			entry, minRow, maxRow, err = halfSeats(entry, minRow, maxRow)
-			// fmt.Println(pass, min, max)
 			if err != nil {
 				return 0, 0, nil, err
 			}
