9e7791f865ae7d4a9c4a42f3c44443a5e9361da1
Sam Littlefair <sam.littlefair@sparx.co.uk>
2021-03-20 16:47:50 +0000

    Split out unto sub functions

--- 2020/Day09/solution.go
+++ 2020/Day09/solution.go
@@ -2,30 +2,40 @@ package main
 
 import (
 	helpers "Advent-of-Code"
+	"errors"
 	"fmt"
 )
 
-func cyclePrevNumbers(entries []int, preambleLength int, i int) bool {
+func cyclePrevNumbers(entries []int, preambleLength int, i int) (bool, int, int) {
 	for j := i - preambleLength; j < i; j++ {
 		for k := i - preambleLength; k < i; k++ {
 			if j != k && entries[j]+entries[k] == entries[i] {
-				return true
+				return false, -1, -1
 			}
 		}
 	}
-	return false
+	return true, i, entries[i]
+}
+
+func part1(entries []int, preambleLength int) (int, int, error) {
+	for i := preambleLength; i < len(entries); i++ {
+		solved, solIndex, solValue := cyclePrevNumbers(entries, preambleLength, i)
+		if solved {
+			return solIndex, solValue, nil
+		}
+	}
+	return -1, -1, errors.New("could not find solution to part 1")
 }
 
 func main() {
 	entries := helpers.ReadFileAsInts()
-
 	preambleLength := 25
 
-	for i := preambleLength; i < len(entries); i++ {
-		moveOn := cyclePrevNumbers(entries, preambleLength, i)
-		if !moveOn {
-			fmt.Println("Part 1:", entries[i])
-			break
-		}
+	_, part1SolValue, err := part1(entries, preambleLength)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+
+	fmt.Println("Part 1:", part1SolValue)
 }
