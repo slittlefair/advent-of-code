c5f9c6b6e6b287c3c8c06d5b67a6d47af995da79
Sam Littlefair <sam.littlefair@sparx.co.uk>
2021-03-25 18:31:18 +0000

    Part 2

--- 2020/Day16/solution.go
+++ 2020/Day16/solution.go
@@ -9,8 +9,32 @@ import (
 )
 
 type ValidNumbers map[int]bool
-type TicketFields map[string]ValidNumbers
+type PossibleRows map[int]bool
+type FieldParams struct {
+	vn ValidNumbers
+	pr PossibleRows
+}
+type TicketFields map[string]FieldParams
 type AllTickets [][]int
+
+func (tf TicketFields) populateField(re *regexp.Regexp, field []string) error {
+	rangeLimits := re.FindAllString(field[1], -1)
+	rangeLimitsNums := helpers.StringSliceToIntSlice(rangeLimits)
+	if len(rangeLimitsNums) != 4 {
+		return errors.New("range limits not as expected")
+	}
+	vn := ValidNumbers{}
+	for i := rangeLimitsNums[0]; i <= rangeLimitsNums[3]; i++ {
+		if i <= rangeLimitsNums[1] || i >= rangeLimitsNums[2] {
+			vn[i] = true
+		}
+	}
+	tf[field[0]] = FieldParams{
+		vn: vn,
+		pr: tf[field[0]].pr,
+	}
+	return nil
+}
 
 func main() {
 	entries := helpers.ReadFile()
@@ -18,50 +42,88 @@ func main() {
 	tFields := TicketFields{}
 	myTicket := []int{}
 	allTickets := AllTickets{}
+	errorRate := 0
 	for _, entry := range entries {
 		if entry == "" || entry == "your ticket:" || entry == "nearby tickets:" {
 			continue
 		}
 		field := strings.Split(entry, ":")
 		if len(field) > 1 {
-			rangeLimits := re.FindAllString(field[1], -1)
-			rangeLimitsNums := helpers.StringSliceToIntSlice(rangeLimits)
-			if len(rangeLimitsNums) != 4 {
-				fmt.Println(errors.New("range limits not as expected"), rangeLimitsNums)
+			if err := tFields.populateField(re, field); err != nil {
+				fmt.Println(err)
+				return
 			}
-			vn := ValidNumbers{}
-			for i := rangeLimitsNums[0]; i <= rangeLimitsNums[3]; i++ {
-				if i <= rangeLimitsNums[1] || i >= rangeLimitsNums[2] {
-					vn[i] = true
-				}
-			}
-			tFields[field[0]] = vn
 		} else {
 			nums := helpers.StringSliceToIntSlice(re.FindAllString(entry, -1))
 			if len(myTicket) == 0 {
 				myTicket = nums
+				for field := range tFields {
+					possibleRows := PossibleRows{}
+					for i := 0; i < len(myTicket); i++ {
+						possibleRows[i] = true
+					}
+					tFields[field] = FieldParams{
+						pr: possibleRows,
+						vn: tFields[field].vn,
+					}
+				}
 			} else {
-				allTickets = append(allTickets, nums)
-			}
-		}
-	}
-
-	errorRate := 0
-
-	for _, ticket := range allTickets {
-		for _, num := range ticket {
-			if isValid := tFields.numIsValid(num); !isValid {
-				errorRate += num
+				validTicket := true
+				for _, num := range nums {
+					if isValid := tFields.numIsValid(num); !isValid {
+						errorRate += num
+						validTicket = false
+					}
+				}
+				if validTicket {
+					allTickets = append(allTickets, nums)
+				}
 			}
 		}
 	}
 
 	fmt.Println("Part 1:", errorRate)
+
+	ticketColToField := make(map[int]string)
+
+	for len(ticketColToField) < len(myTicket) {
+		for _, ticket := range allTickets {
+			for j, num := range ticket {
+				for field, param := range tFields {
+					if !param.vn[num] {
+						delete(tFields[field].pr, j)
+					}
+					if len(tFields[field].pr) == 1 {
+						// There should only be one so the loop only runs once
+						for key := range tFields[field].pr {
+							ticketColToField[key] = field
+							for f := range tFields {
+								if f != field {
+									delete(tFields[f].pr, key)
+								}
+							}
+						}
+
+					}
+				}
+			}
+		}
+	}
+
+	runningTotal := 1
+	for i, val := range myTicket {
+		if strings.HasPrefix(ticketColToField[i], "departure") {
+			runningTotal *= val
+		}
+	}
+
+	fmt.Println("Part 2:", runningTotal)
+
 }
 
 func (tf TicketFields) numIsValid(num int) bool {
 	for _, field := range tf {
-		if field[num] {
+		if field.vn[num] {
 			return true
 		}
 	}
