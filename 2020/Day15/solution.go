431aecfe271753f329375f692eeead2ef4d423c2
slittlefair <sam.littlefair@sparx.co.uk>
2021-04-05 16:09:31 +0100

    2020/day15 (#12)

---
+++ 2020/Day15/solution.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	helpers "Advent-of-Code"
+	"fmt"
+	"regexp"
+)
+
+type NumsSaid struct {
+	lastSaid        int
+	penultimateSaid int
+}
+
+type WhenNumsSaid map[int]NumsSaid
+
+var lastNumSaid int
+
+func (wns WhenNumsSaid) parseInput(numStrings []int) int {
+	lastNumSaid := 0
+	for i, num := range numStrings {
+		wns[num] = NumsSaid{
+			lastSaid: i + 1,
+		}
+		lastNumSaid = num
+	}
+	return lastNumSaid
+}
+
+func (wns WhenNumsSaid) playGame(startingIndex int) (int, int) {
+	var part1Sol int
+	for i := startingIndex; i <= 30000000; i++ {
+		if wns[lastNumSaid].penultimateSaid == 0 {
+			lastNumSaid = 0
+		} else {
+			lastNumSaid = wns[lastNumSaid].lastSaid - wns[lastNumSaid].penultimateSaid
+		}
+		if _, ok := wns[lastNumSaid]; !ok {
+			wns[lastNumSaid] = NumsSaid{
+				lastSaid: i,
+			}
+		} else {
+			wns[lastNumSaid] = NumsSaid{
+				penultimateSaid: wns[lastNumSaid].lastSaid,
+				lastSaid:        i,
+			}
+		}
+		if i == 2020 {
+			part1Sol = lastNumSaid
+		}
+	}
+	return part1Sol, lastNumSaid
+}
+
+func main() {
+	numList := helpers.ReadFile()[0]
+	re := regexp.MustCompile(`\d+`)
+	numStrings := helpers.StringSliceToIntSlice(re.FindAllString(numList, -1))
+	whenNumsSaid := WhenNumsSaid{}
+	lastNumSaid = whenNumsSaid.parseInput(numStrings)
+	part1Sol, part2Sol := whenNumsSaid.playGame(len(numStrings) + 1)
+	fmt.Println("Part 1:", part1Sol)
+	fmt.Println("Part 2:", part2Sol)
+}
