cdc80ca44d3956655afeed95b43418894abdb55f
Sam Littlefair <sam.littlefair@sparx.co.uk>
2021-04-30 23:40:55 +0100

    Function comments

--- 2020/Day20/picture/picture.go
+++ 2020/Day20/picture/picture.go
@@ -19,6 +19,38 @@ type Picture struct {
 
 // TODO add comments to exported functions
 
+// Populate tiles takes the string of lines, input, and populates the tiles accordingly
+func (p *Picture) PopulateTiles(input []string) {
+	re := regexp.MustCompile(`\d+`)
+	t := tile.Tile{
+		Pixels: make(map[helpers.Co]string),
+	}
+	var i int
+	for _, line := range input {
+		if line == "" {
+			p.Tiles = append(p.Tiles, t)
+			t = tile.Tile{
+				Pixels: make(map[helpers.Co]string),
+			}
+			continue
+		}
+		if match := re.FindString(line); match != "" {
+			t.ID = match
+			i = 0
+			continue
+		}
+		t.Height = i
+		t.Width = len(line) - 1
+		for j, char := range line {
+			t.Pixels[helpers.Co{X: j, Y: i}] = string(char)
+		}
+		i++
+	}
+	p.Tiles = append(p.Tiles, t)
+}
+
+// FindMatchesForTile finds any tiles that are adjacent to tile t and sets them as either "top",
+// "bottom", "left" or "right" on the respective tiles
 func (p Picture) FindMatchesForTile(t tile.Tile, index int) {
 	for k, tile := range p.Tiles {
 		if tile.ID == t.ID || t.IsAdjacentTo(tile) || t.NumAdjacent() == 4 || tile.NumAdjacent() == 4 {
@@ -61,35 +93,9 @@ func (p Picture) FindMatchesForTile(t tile.Tile, index int) {
 	}
 }
 
-func (p *Picture) PopulateTiles(input []string) {
-	re := regexp.MustCompile(`\d+`)
-	t := tile.Tile{
-		Pixels: make(map[helpers.Co]string),
-	}
-	var i int
-	for _, line := range input {
-		if line == "" {
-			p.Tiles = append(p.Tiles, t)
-			t = tile.Tile{
-				Pixels: make(map[helpers.Co]string),
-			}
-			continue
-		}
-		if match := re.FindString(line); match != "" {
-			t.ID = match
-			i = 0
-			continue
-		}
-		t.Height = i
-		t.Width = len(line) - 1
-		for j, char := range line {
-			t.Pixels[helpers.Co{X: j, Y: i}] = string(char)
-		}
-		i++
-	}
-	p.Tiles = append(p.Tiles, t)
-}
-
+// CalculateCornerIDs returns the product of the IDs for the four corner tile in picture p. Since
+// the IDs are kept as strings they need to be converted to ints, so we return an error if this
+// conversion can't be done. The value returned here is the solution to Part 1 of the challenge
 func (p Picture) CalculateCornerIDs() (int, error) {
 	cornerID := 1
 	for _, tile := range p.Tiles {
@@ -104,6 +110,8 @@ func (p Picture) CalculateCornerIDs() (int, error) {
 	return cornerID, nil
 }
 
+// getTileFromID returns the tile that has the given ID. If no such tile exists then return an
+// error, as something has obviously gone wrong.
 func (p Picture) getTileFromID(id string) (tile.Tile, error) {
 	for _, tile := range p.Tiles {
 		if tile.ID == id {
@@ -113,6 +121,8 @@ func (p Picture) getTileFromID(id string) (tile.Tile, error) {
 	return tile.Tile{}, errors.New(fmt.Sprintln("could not find tile for id:", id))
 }
 
+// getTopeLeftTile returns the tile at the top left of the picture. We do this so we can start
+// creating a picture from this tile, with all other tiles relative to it.
 func (p Picture) getTopLeftTile() (tile.Tile, error) {
 	for _, t := range p.Tiles {
 		if t.AdjacentTiles.Bottom != "" && t.AdjacentTiles.Right != "" && t.AdjacentTiles.Left == "" && t.AdjacentTiles.Top == "" {
@@ -122,6 +132,8 @@ func (p Picture) getTopLeftTile() (tile.Tile, error) {
 	return tile.Tile{}, errors.New("no tile found at top left")
 }
 
+// populatePictureWithTile gets the individual tiles and puts their pixels into the picture p's
+// pixels, into one large picture
 func (p *Picture) populatePictureWithTile(t tile.Tile, x, y int) {
 	p.TileMap[helpers.Co{X: x, Y: y}] = t
 	for i := 1; i < t.Height; i++ {
@@ -140,6 +152,8 @@ func (p *Picture) populatePictureWithTile(t tile.Tile, x, y int) {
 	}
 }
 
+// PopulateTileMap gets the individual tiles and determines their relativity to each other, in order
+// to create a single set of pixels
 func (p *Picture) PopulateTileMap() error {
 	// Find the top left tile first, then build out from there
 	var x, y int
@@ -174,6 +188,8 @@ func (p *Picture) PopulateTileMap() error {
 	}
 }
 
+// PrintPictureMap is a helper method which prints a visual representation of the pixels of a tile
+// in the console
 func (p Picture) PrintPictureMap() {
 	for h := 0; h <= p.Height; h++ {
 		for w := 0; w <= p.Width; w++ {
@@ -201,6 +217,7 @@ var seaMonster = []helpers.Co{
 	{X: 19, Y: 1},
 }
 
+// rotatePicture90 rotates the pixels in the picture p by 90 degrees
 func (p *Picture) rotatePicture90() {
 	newPixels := make(map[helpers.Co]string)
 	for co, val := range p.Pixels {
@@ -209,6 +226,7 @@ func (p *Picture) rotatePicture90() {
 	p.Pixels = newPixels
 }
 
+// flipPicture flips (reflects) the pixels of a tile along the vertical centre
 func (p *Picture) flipPicture() {
 	newPixels := make(map[helpers.Co]string)
 	for co, val := range p.Pixels {
@@ -217,6 +235,10 @@ func (p *Picture) flipPicture() {
 	p.Pixels = newPixels
 }
 
+// FindSeaMonster iterates through the picture p and finds any sea monsters. They only exist on one
+// orientation of the picture, so if we find at least one then we can find the rest in that
+// orientation then return early. Otherwise we keep rotating the picture, then flip it before
+// rotating it again.
 func (p *Picture) FindSeaMonster() {
 	var found bool
 	for j := 0; j < 2; j++ {
@@ -235,6 +257,8 @@ func (p *Picture) FindSeaMonster() {
 	}
 }
 
+// checkSeaMonsterAtCo checks to see if a sea monster is in the picture relative to the given
+// coordinate, co
 func (p *Picture) checkSeaMonsterAtCo(co helpers.Co) bool {
 	for _, smCo := range seaMonster {
 		c := helpers.Co{X: co.X + smCo.X, Y: co.Y + smCo.Y}
@@ -246,12 +270,16 @@ func (p *Picture) checkSeaMonsterAtCo(co helpers.Co) bool {
 	return true
 }
 
+// markSeaMonster changes the pixels in the picture that represent a sea monster from "#" to "O"
 func (p *Picture) markSeaMonster(co helpers.Co) {
 	for _, smCo := range seaMonster {
 		p.Pixels[helpers.Co{X: co.X + smCo.X, Y: co.Y + smCo.Y}] = "O"
 	}
 }
 
+// CountWaterRoughness returns the number of rough patches of water "#" in the picture. The value
+// returned here is the solution to Part 2 of the challenge, as long as it's done after we exclude
+// all sea monster pixels via markSeaMonster
 func (p Picture) CountWaterRoughness() int {
 	count := 0
 	for _, val := range p.Pixels {
--- 2020/Day20/tile/tile.go
+++ 2020/Day20/tile/tile.go
@@ -21,6 +21,8 @@ type Tile struct {
 	InPlace       bool
 }
 
+// PrintPixels is a helper method which prints a visual representation of the pixels of a tile in
+// the console
 func (t Tile) PrintPixels() {
 	for h := 0; h <= t.Height; h++ {
 		for w := 0; w <= t.Width; w++ {
@@ -30,8 +32,7 @@ func (t Tile) PrintPixels() {
 	}
 }
 
-// TODO add comments to exported functions
-
+// RotateTile90 rotates the pixels of a tile by 90 degrees
 func (t *Tile) RotateTile90() {
 	newPixels := make(map[helpers.Co]string)
 	for co, val := range t.Pixels {
@@ -40,6 +41,7 @@ func (t *Tile) RotateTile90() {
 	t.Pixels = newPixels
 }
 
+// FlipTile flips (reflects) the pixels of a tile along the vertical centre
 func (t *Tile) FlipTile() {
 	newPixels := make(map[helpers.Co]string)
 	for co, val := range t.Pixels {
@@ -48,6 +50,7 @@ func (t *Tile) FlipTile() {
 	t.Pixels = newPixels
 }
 
+// IsAdjacentTop returns whether the tile "tile" is directly above the tile "t"
 func (t Tile) IsAdjacentTop(tile Tile) bool {
 	for x := 0; x <= t.Width; x++ {
 		if t.Pixels[helpers.Co{X: x, Y: t.Height}] != tile.Pixels[helpers.Co{X: x, Y: 0}] {
@@ -57,6 +60,7 @@ func (t Tile) IsAdjacentTop(tile Tile) bool {
 	return true
 }
 
+// IsAdjacentTop returns whether the tile "tile" is directly below the tile "t"
 func (t Tile) IsAdjacentBottom(tile Tile) bool {
 	for x := 0; x <= t.Width; x++ {
 		if t.Pixels[helpers.Co{X: x, Y: 0}] != tile.Pixels[helpers.Co{X: x, Y: t.Height}] {
@@ -66,6 +70,7 @@ func (t Tile) IsAdjacentBottom(tile Tile) bool {
 	return true
 }
 
+// IsAdjacentTop returns whether the tile "tile" is directly to the left of tile "t"
 func (t Tile) IsAdjacentLeft(tile Tile) bool {
 	for y := 0; y <= t.Height; y++ {
 		if t.Pixels[helpers.Co{X: 0, Y: y}] != tile.Pixels[helpers.Co{X: tile.Width, Y: y}] {
@@ -75,6 +80,7 @@ func (t Tile) IsAdjacentLeft(tile Tile) bool {
 	return true
 }
 
+// IsAdjacentTop returns whether the tile "tile" is directly to the right of tile "t"
 func (t Tile) IsAdjacentRight(tile Tile) bool {
 	for y := 0; y <= t.Height; y++ {
 		if t.Pixels[helpers.Co{X: t.Width, Y: y}] != tile.Pixels[helpers.Co{X: 0, Y: y}] {
@@ -84,6 +90,7 @@ func (t Tile) IsAdjacentRight(tile Tile) bool {
 	return true
 }
 
+// isAdjacentTo returns whether the tile "tile" is adjacent to the tile "t"
 func (t Tile) IsAdjacentTo(tile Tile) bool {
 	if t.AdjacentTiles.Top == tile.ID {
 		return true
@@ -100,6 +107,7 @@ func (t Tile) IsAdjacentTo(tile Tile) bool {
 	return false
 }
 
+// NumAdjacent returns how many tiles are adjacent to the tile "t"
 func (t Tile) NumAdjacent() int {
 	numAdjacent := 0
 	if t.AdjacentTiles.Top != "" {
