45bbf96348fa301cb106d820e89a530d5f80c30c
Sam Littlefair <sam.littlefair@sparx.co.uk>
2021-05-10 16:56:37 +0100

    2015/Day04

--- 2015/Day04/solution.go
+++ 2015/Day04/solution.go
@@ -1,44 +1,38 @@
 package main
 
 import (
+	helpers "Advent-of-Code"
 	"crypto/md5"
-	"encoding/hex"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
-var input = "ckczppom"
+func hashIsValidPart1(hash string) bool {
+	return strings.HasPrefix(hash, "00000")
+}
 
-func main() {
-	i := 1
-	var part1Solution int
-	var part2Solution int
+func hashIsValidPart2(hash string) bool {
+	return strings.HasPrefix(hash, "000000")
+}
+
+func findValidHash(input string, part1 bool) int {
+	i := 0
 	for {
-		code := input + strconv.Itoa(i)
-		byteConversion := md5.Sum([]byte(code))
-		conversion := hex.EncodeToString(byteConversion[:])
-		foundPart1Solution := true
-		foundPart2Solution := true
-		for _, l := range conversion[:5] {
-			if string(l) != "0" {
-				foundPart1Solution = false
-				foundPart2Solution = false
-			}
+		str := input + strconv.Itoa(i)
+		hash := fmt.Sprintf("%x", md5.Sum([]byte(str)))
+		if part1 && hashIsValidPart1(hash) {
+			return i
 		}
-		if string(conversion[5]) != "0" {
-			foundPart2Solution = false
-		}
-		if foundPart1Solution && part1Solution == 0 {
-			part1Solution = i
-		}
-		if foundPart2Solution {
-			part2Solution = i
-		}
-		if foundPart1Solution && foundPart2Solution {
-			break
+		if !part1 && hashIsValidPart2(hash) {
+			return i
 		}
 		i++
 	}
-	fmt.Println("Part 1:", part1Solution)
-	fmt.Println("Part 2:", part2Solution)
+}
+
+func main() {
+	input := helpers.ReadFile()[0]
+	fmt.Println("Part 1:", findValidHash(input, true))
+	fmt.Println("Part 2:", findValidHash(input, false))
 }
---
+++ 2015/Day04/solution_test.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_hashIsValidPart1(t *testing.T) {
+	tests := []struct {
+		name string
+		hash string
+		want bool
+	}{
+		{
+			name: "returns true if the hash starts with five zeroes",
+			hash: "00000abcd",
+			want: true,
+		},
+		{
+			name: "returns true if the hash starts with five zeroes, advent of code example 1",
+			hash: "000001dbbfa",
+			want: true,
+		},
+		{
+			name: "returns true if the hash starts with five zeroes, advent of code example 2",
+			hash: "000006136ef",
+			want: true,
+		},
+		{
+			name: "returns false if the hash doesn't start with five zeroes",
+			hash: "0000abcd",
+			want: false,
+		},
+		{
+			name: "returns false if the hash doesn't start with any zeroes",
+			hash: "124abcd",
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hashIsValidPart1(tt.hash); got != tt.want {
+				t.Errorf("hashIsValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_hashIsValidPart2(t *testing.T) {
+	tests := []struct {
+		name string
+		hash string
+		want bool
+	}{
+		{
+			name: "returns true if the hash starts with six zeroes",
+			hash: "000000abcd",
+			want: true,
+		},
+		{
+			name: "returns false if the hash doesn't start with six zeroes",
+			hash: "00000abcd",
+			want: false,
+		},
+		{
+			name: "returns false if the hash doesn't start with any zeroes",
+			hash: "124abcd",
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hashIsValidPart2(tt.hash); got != tt.want {
+				t.Errorf("hashIsValidPart2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_findValidHash(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		part1 bool
+		want  int
+	}{
+		{
+			name:  "returns solution to code abcdef, advent of code example 1, part 1",
+			input: "abcdef",
+			part1: true,
+			want:  609043,
+		},
+		{
+			name:  "returns solution to code abcdef, advent of code example 2, part 1",
+			input: "pqrstuv",
+			part1: true,
+			want:  1048970,
+		},
+		{
+			name:  "returns solution to code abcdef, advent of code example 1, part 2",
+			input: "abcdef",
+			part1: false,
+			want:  6742839,
+		},
+		{
+			name:  "returns solution to code abcdef, advent of code example 2, part 2",
+			input: "pqrstuv",
+			part1: false,
+			want:  5714438,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findValidHash(tt.input, tt.part1); got != tt.want {
+				t.Errorf("findValidHash() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
