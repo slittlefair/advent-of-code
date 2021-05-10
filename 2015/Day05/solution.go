c98c79982dbe43f8cde39f07c120da9b1ed5d97a
Sam Littlefair <sam.littlefair@sparx.co.uk>
2021-05-10 22:17:01 +0100

    2015/Day05

--- 2015/Day05/solution.go
+++ 2015/Day05/solution.go
@@ -1,85 +1,113 @@
 package main
 
 import (
-	"Advent-of-Code"
+	helpers "Advent-of-Code"
 	"fmt"
+	"regexp"
 )
 
-func part1(strings []string) (numNiceStrings int) {
-	vowels := [5]string{"a", "e", "i", "o", "u"}
-	naughtyStrings := [4]string{"ab", "cd", "pq", "xy"}
-	for _, s := range strings {
-		naughty := false
-		numVowels := 0
-		doubleLetter := false
-		for i, l := range s {
-			letter := string(l)
-			for _, j := range vowels {
-				if j == letter {
-					numVowels++
-				}
-			}
-			if i < len(s)-1 {
-				if letter == string(s[i+1]) {
-					doubleLetter = true
-				}
-				for _, ns := range naughtyStrings {
-					if letter+string(s[i+1]) == ns {
-						naughty = true
-					}
-				}
-			}
-		}
-		if numVowels < 3 || !doubleLetter {
-			naughty = true
-		}
-		if !naughty {
-			numNiceStrings++
-		}
-	}
-	return numNiceStrings
+func containsAtLeast3Vowels(re *regexp.Regexp, str string) bool {
+	return len(re.FindAllString(str, -1)) >= 3
 }
 
-func part2(strings []string) (numNiceStrings int) {
-	for _, s := range strings {
-		doubles := make(map[string]int)
-		hasDoubles := false
-		gapRepeats := false
-		for i := range s {
-			if i < len(s)-2 {
-				if s[i] == s[i+2] {
-					gapRepeats = true
-				}
-			}
-		}
-		for i := 0; i < len(s); i++ {
-			if i <= len(s)-2 {
-				if freq, ok := doubles[string(s[i])+string(s[i+1])]; !ok {
-					doubles[string(s[i])+string(s[i+1])] = 1
-				} else {
-					doubles[string(s[i])+string(s[i+1])] = freq + 1
-				}
-				if i < len(s)-2 && s[i] == s[i+1] && s[i] == s[i+2] {
-					i++
-				}
-			}
-
-		}
-		for _, freq := range doubles {
-			if freq > 1 {
-				hasDoubles = true
-			}
-		}
-		if gapRepeats && hasDoubles {
-			numNiceStrings++
+func containsDoubleLetter(str string) bool {
+	for i := 0; i < len(str)-1; i++ {
+		if str[i] == str[i+1] {
+			return true
 		}
 	}
+	return false
+}
 
-	return numNiceStrings
+func doesNotContainBadString(re *regexp.Regexp, str string) bool {
+	return !re.MatchString(str)
+}
+
+func doesNotContainAnyBadStrings(reGroup []*regexp.Regexp, str string) bool {
+	for _, re := range reGroup {
+		if !doesNotContainBadString(re, str) {
+			return false
+		}
+	}
+	return true
+}
+
+func isNicePart1(vowelRe *regexp.Regexp, badStringRe []*regexp.Regexp, str string) bool {
+	if !containsAtLeast3Vowels(vowelRe, str) {
+		return false
+	}
+	if !containsDoubleLetter(str) {
+		return false
+	}
+	if !doesNotContainAnyBadStrings(badStringRe, str) {
+		return false
+	}
+	return true
+}
+
+func containsRepeatedPairOfLetters(str string) bool {
+	letterPairs := map[string]int{}
+	for i := 0; i < len(str)-1; i++ {
+		letterPair := fmt.Sprintf("%b%b", str[i], str[i+1])
+		if _, ok := letterPairs[letterPair]; !ok {
+			letterPairs[letterPair] = 1
+		} else {
+			letterPairs[letterPair]++
+		}
+		if i < len(str)-2 && str[i] == str[i+1] && str[i] == str[i+2] {
+			i++
+		}
+	}
+	for _, val := range letterPairs {
+		if val > 1 {
+			return true
+		}
+	}
+	return false
+}
+
+func repeatsLetterWithGap(str string) bool {
+	for i := 0; i < len(str)-2; i++ {
+		if str[i] == str[i+2] {
+			return true
+		}
+	}
+	return false
+}
+
+func isNicePart2(str string) bool {
+	if !containsRepeatedPairOfLetters(str) {
+		return false
+	}
+	if !repeatsLetterWithGap(str) {
+		return false
+	}
+	return true
+}
+
+func getNiceStringCount(input []string) (int, int) {
+	var part1Count, part2Count int
+	vowelRe := regexp.MustCompile(`a|e|i|o|u`)
+	badStringRe := []*regexp.Regexp{
+		regexp.MustCompile(`ab`),
+		regexp.MustCompile(`cd`),
+		regexp.MustCompile(`pq`),
+		regexp.MustCompile(`xy`),
+	}
+	for _, str := range input {
+		if isNicePart1(vowelRe, badStringRe, str) {
+			part1Count++
+		}
+		if isNicePart2(str) {
+			part2Count++
+		}
+	}
+	return part1Count, part2Count
 }
 
 func main() {
-	strings := helpers.ReadFile()
-	fmt.Println("Part 1:", part1(strings))
-	fmt.Println("Part 2:", part2(strings))
+	input := helpers.ReadFile()
+	part1Count, part2Count := getNiceStringCount(input)
+	fmt.Println("Part 1:", part1Count)
+	fmt.Println("Part 2:", part2Count)
 }
