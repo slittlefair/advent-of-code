514e911828edbb98b2963787a2cbe8f355a63505
Sam Littlefair <sam.littlefair@sparx.co.uk>
2021-05-10 11:32:18 +0100

    2015/Day01

--- 2015/Day01/solution.go
+++ 2015/Day01/solution.go
@@ -1,25 +1,44 @@
 package main
 
 import (
-	"Advent-of-Code"
+	helpers "Advent-of-Code"
 	"fmt"
 )
 
-func main() {
-	instructions := helpers.ReadFile()[0]
-	currentFloor := 0
-	part2Sol := 0
-	for i, r := range instructions {
-		char := string(r)
-		if char == "(" {
+func getFloorFromInstructions(instructions string) int {
+	var currentFloor int
+	for _, str := range instructions {
+		if string(str) == "(" {
 			currentFloor++
 		} else {
 			currentFloor--
 		}
-		if currentFloor == -1 && part2Sol == 0 {
-			part2Sol = i + 1
+	}
+	return currentFloor
+}
+
+func getFirstInstanceOfBasement(instructions string) (int, error) {
+	var currentFloor int
+	for i, str := range instructions {
+		if string(str) == "(" {
+			currentFloor++
+		} else {
+			currentFloor--
+		}
+		if currentFloor == -1 {
+			return i + 1, nil
 		}
 	}
-	fmt.Println("Part 1:", currentFloor)
-	fmt.Println("Part 2:", part2Sol)
+	return -1, fmt.Errorf("did not get to basement")
+}
+
+func main() {
+	instructions := helpers.ReadFile()[0]
+	fmt.Println("Part 1:", getFloorFromInstructions(instructions))
+	basementIndex, err := getFirstInstanceOfBasement(instructions)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Part 2:", basementIndex)
 }
