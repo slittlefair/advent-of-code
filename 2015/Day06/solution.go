e9ee818ce83aaca028deea619c695c773760d0c5
slittlefair <sam.littlefair@sparx.co.uk>
2021-05-11 11:07:11 +0100

    2015/day06 (#32)

--- 2015/Day06/solution.go
+++ 2015/Day06/solution.go
@@ -1,76 +1,112 @@
 package main
 
 import (
-	"Advent-of-Code"
+	helpers "Advent-of-Code"
 	"fmt"
 	"regexp"
 )
 
-type coordinate struct {
-	X int
-	Y int
+type Lights struct {
+	Analogue map[helpers.Co]bool
+	Digital  map[helpers.Co]int
 }
 
-var part1Lights = make(map[coordinate]bool)
-var part2Lights = make(map[coordinate]int)
-
-func updateStatus(x int, y int, instruction string) {
-	co := coordinate{x, y}
-	switch instruction {
-	case "on":
-		part1Lights[co] = true
-	case "off":
-		part1Lights[co] = false
-	case "through":
-		part1Lights[co] = !part1Lights[co]
+func populateLights() *Lights {
+	lights := Lights{
+		Analogue: make(map[helpers.Co]bool),
+		Digital:  make(map[helpers.Co]int),
 	}
-}
-
-func updateBrightness(x int, y int, instruction string) {
-	co := coordinate{x, y}
-	switch instruction {
-	case "on":
-		part2Lights[co] = part2Lights[co] + 1
-	case "off":
-		if part2Lights[co] > 0 {
-			part2Lights[co] = part2Lights[co] - 1
-		}
-	case "through":
-		part2Lights[co] = part2Lights[co] + 2
-	}
-}
-
-func main() {
 	for x := 0; x < 1000; x++ {
 		for y := 0; y < 1000; y++ {
-			part1Lights[coordinate{x, y}] = false
-			part2Lights[coordinate{x, y}] = 0
+			lights.Analogue[helpers.Co{X: x, Y: y}] = false
+			lights.Digital[helpers.Co{X: x, Y: y}] = 0
 		}
 	}
+	return &lights
+}
 
-	instructions := helpers.ReadFile()
-	coordsRe := regexp.MustCompile("\\d+")
-	wordsRe := regexp.MustCompile("[a-z]+")
+func (l Lights) turnLightsOn(nums []int) {
+	for x := nums[0]; x <= nums[2]; x++ {
+		for y := nums[1]; y <= nums[3]; y++ {
+			co := helpers.Co{X: x, Y: y}
+			l.Analogue[co] = true
+			l.Digital[co]++
+		}
+	}
+}
 
-	for _, inst := range instructions {
-		points := helpers.StringSliceToIntSlice(coordsRe.FindAllString(inst, -1))
-		words := wordsRe.FindAllString(inst, -1)
-		for x := points[0]; x <= points[2]; x++ {
-			for y := points[1]; y <= points[3]; y++ {
-				updateStatus(x, y, words[1])
-				updateBrightness(x, y, words[1])
+func (l Lights) turnLightsOff(nums []int) {
+	for x := nums[0]; x <= nums[2]; x++ {
+		for y := nums[1]; y <= nums[3]; y++ {
+			co := helpers.Co{X: x, Y: y}
+			l.Analogue[co] = false
+			if l.Digital[co] > 0 {
+				l.Digital[co]--
 			}
 		}
 	}
+}
 
-	totalOn := 0
-	totalBrightness := 0
-	for co := range part1Lights {
-		if part1Lights[co] {
-			totalOn++
+func (l Lights) toggleLights(nums []int) {
+	for x := nums[0]; x <= nums[2]; x++ {
+		for y := nums[1]; y <= nums[3]; y++ {
+			co := helpers.Co{X: x, Y: y}
+			l.Analogue[co] = !l.Analogue[co]
+			l.Digital[co] += 2
 		}
-		totalBrightness += part2Lights[co]
 	}
-	fmt.Println("Part 1:", totalOn)
-	fmt.Println("Part 2:", totalBrightness)
+}
+
+func (l *Lights) followInstructions(input []string) error {
+	re := regexp.MustCompile(`\d+`)
+	reOn := regexp.MustCompile(`on`)
+	reOff := regexp.MustCompile(`off`)
+	reToggle := regexp.MustCompile(`toggle`)
+	for _, inst := range input {
+		nums := re.FindAllString(inst, -1)
+		if len(nums) != 4 {
+			return fmt.Errorf("something went wrong, got nums %v", nums)
+		}
+		intNums := helpers.StringSliceToIntSlice(nums)
+		if reOn.MatchString(inst) {
+			l.turnLightsOn(intNums)
+			continue
+		}
+		if reOff.MatchString(inst) {
+			l.turnLightsOff(intNums)
+			continue
+		}
+		if reToggle.MatchString(inst) {
+			l.toggleLights(intNums)
+			continue
+		}
+		return fmt.Errorf("something went wrong, got instruction %s", inst)
+	}
+	return nil
+}
+
+func (l *Lights) countAnalogueBrightness() int {
+	count := 0
+	for _, light := range l.Analogue {
+		if light {
+			count++
+		}
+	}
+	return count
+}
+
+func (l *Lights) countDigitalBrightness() int {
+	count := 0
+	for _, light := range l.Digital {
+		count += light
+	}
+	return count
+}
+
+func main() {
+	input := helpers.ReadFile()
+	lights := populateLights()
+	lights.followInstructions(input)
+	fmt.Println("Part 1:", lights.countAnalogueBrightness())
+	fmt.Println("Part 2:", lights.countDigitalBrightness())
 }
