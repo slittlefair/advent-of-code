07e385dc81000a80c6f8cb4ed7c54d1d44388ff3
Sam Littlefair <sam.littlefair@sparx.co.uk>
2021-06-10 10:07:47 +0100

    Part 2

--- 2015/Day19/solution.go
+++ 2015/Day19/solution.go
@@ -4,6 +4,7 @@ import (
 	helpers "Advent-of-Code"
 	"fmt"
 	"strings"
+	"unicode"
 )
 
 type Replacements map[string][]string
@@ -59,9 +60,33 @@ func (m *Medicine) ReplaceAndFindNewMolecules() {
 	}
 }
 
+func countUpper(s string) int {
+	count := 0
+	for _, char := range s {
+		if unicode.IsUpper(char) {
+			count++
+		}
+	}
+	return count
+}
+
+// Each "token" ("H", "O", "Ci" etc.) in the molecule can be converted via a replacement except for
+// "Rn", "Ar", and "Y". So to reverse engineer from molecule to "e" we just need to keep replacing
+// tokens to shorten the length. We can do this to all except for those special tokens, so get the
+// number of tokens (each token starts with a capital so count the number of upper case characters),
+// subtract the number of Rn and Ar, subtract 2 lots of Y to include the characters that follow
+// them, and subtract 1 since we start with e.
+func (m Medicine) GetNumberOfSubs() int {
+	RnCount := strings.Count(m.Molecule, "Rn")
+	ArCount := strings.Count(m.Molecule, "Ar")
+	YCount := strings.Count(m.Molecule, "Y")
+	return countUpper(m.Molecule) - RnCount - ArCount - 2*YCount - 1
+}
+
 func main() {
 	input := helpers.ReadFile()
 	med := parseInput(input)
 	med.ReplaceAndFindNewMolecules()
 	fmt.Println("Part 1:", len(med.DistinctNewMolecules))
+	fmt.Println("Part 2:", med.GetNumberOfSubs())
 }
