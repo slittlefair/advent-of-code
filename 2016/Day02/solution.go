4380ab54994f04a038cb41c81819638ee831c3db
Sam Littlefair <sam.littlefair@sparx.co.uk>
2021-07-25 22:05:11 +0100

    Day02 solution

---
+++ 2016/Day02/solution.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	helpers "Advent-of-Code"
+	"fmt"
+)
+
+type CodeConstructor struct {
+	currentCo helpers.Co
+	code      []string
+}
+
+func (cc *CodeConstructor) move(dir string, keypad map[helpers.Co]string) {
+	newCo := helpers.Co{X: cc.currentCo.X, Y: cc.currentCo.Y}
+	if dir == "U" {
+		newCo.Y--
+	}
+	if dir == "D" {
+		newCo.Y++
+	}
+	if dir == "L" {
+		newCo.X--
+	}
+	if dir == "R" {
+		newCo.X++
+	}
+	if _, ok := keypad[newCo]; ok {
+		cc.currentCo = newCo
+	}
+}
+
+func (cc *CodeConstructor) followDirections(line string, keypad map[helpers.Co]string) {
+	for _, d := range line {
+		cc.move(string(d), keypad)
+	}
+	cc.code = append(cc.code, keypad[cc.currentCo])
+}
+
+func (cc CodeConstructor) getCode() string {
+	code := ""
+	for _, c := range cc.code {
+		code += c
+	}
+	return code
+}
+
+func getSolution(input []string, keypad map[helpers.Co]string, startingCo helpers.Co) string {
+	cc := &CodeConstructor{
+		currentCo: startingCo,
+	}
+	for _, line := range input {
+		cc.followDirections(line, keypad)
+	}
+	return cc.getCode()
+}
+
+func main() {
+	input := helpers.ReadFile()
+	keypad := map[helpers.Co]string{
+		{X: 0, Y: 0}: "1",
+		{X: 1, Y: 0}: "2",
+		{X: 2, Y: 0}: "3",
+		{X: 0, Y: 1}: "4",
+		{X: 1, Y: 1}: "5",
+		{X: 2, Y: 1}: "6",
+		{X: 0, Y: 2}: "7",
+		{X: 1, Y: 2}: "8",
+		{X: 2, Y: 2}: "9",
+	}
+	fmt.Println("Part 1:", getSolution(input, keypad, helpers.Co{X: 1, Y: 1}))
+	keypad = map[helpers.Co]string{
+		{X: 2, Y: 0}: "1",
+		{X: 1, Y: 1}: "2",
+		{X: 2, Y: 1}: "3",
+		{X: 3, Y: 1}: "4",
+		{X: 0, Y: 2}: "5",
+		{X: 1, Y: 2}: "6",
+		{X: 2, Y: 2}: "7",
+		{X: 3, Y: 2}: "8",
+		{X: 4, Y: 2}: "9",
+		{X: 1, Y: 3}: "A",
+		{X: 2, Y: 3}: "B",
+		{X: 3, Y: 3}: "C",
+		{X: 2, Y: 4}: "D",
+	}
+	fmt.Println("Part 2:", getSolution(input, keypad, helpers.Co{X: 0, Y: 2}))
+}
--- 2016/README.md
+++ 2016/README.md
@@ -1,3 +1,4 @@
 # :christmas_tree: 2016 :christmas_tree:
 
 - [x] Day 1 - No Time for a Taxicab :alarm_clock: :taxi:
+- [x] Day 2 - Bathroom Security :bathtub: :police_officer: 
