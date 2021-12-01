cf8099937dc9e1656773652bba6b40a5acf757c8
Sam Littlefair <sam.littlefair@sparx.co.uk>
2021-12-01 21:21:35 +0000

    2021 let's go!

---
+++ 2021/Day01/solution.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	helpers "Advent-of-Code"
+	"fmt"
+)
+
+func calculateIncreases(input []int) int {
+	numIncreases := 0
+	for i := 0; i < len(input)-1; i++ {
+		if input[i] < input[i+1] {
+			numIncreases++
+		}
+	}
+	return numIncreases
+}
+
+func calculateSlidingWindows(input []int) []int {
+	windows := []int{}
+	for i := 0; i < len(input)-2; i++ {
+		windows = append(windows, input[i]+input[i+1]+input[i+2])
+	}
+	return windows
+}
+
+func part1(input []int) int {
+	return calculateIncreases(input)
+}
+
+func part2(input []int) int {
+	return calculateIncreases(calculateSlidingWindows((input)))
+}
+
+func main() {
+	input := helpers.ReadFileAsInts()
+	fmt.Println("Part 1:", part1(input))
+	fmt.Println("Part 2:", part2(input))
+}
