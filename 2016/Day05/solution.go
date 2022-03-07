cae3ac038f5ff830a7ae9bcb1b9c720bd68e3db6
slittlefair <sam.littlefair@sparx.co.uk>
2022-03-07 15:44:57 +0000

    Add emojis to printed output (#86)

--- 2016/Day05/solution.go
+++ 2016/Day05/solution.go
@@ -4,6 +4,7 @@ import (
 	"Advent-of-Code/file"
 	"crypto/md5"
 	"fmt"
+	"html"
 	"strconv"
 	"strings"
 )
@@ -37,6 +38,24 @@ func printPassword(part int, password [8]string) string {
 	return p
 }
 
+func getEmoji(unicode string) string {
+	xx := fmt.Sprintf("\\U000%s", unicode)
+	// Hex String
+	h := strings.ReplaceAll(xx, "\\U", "0x")
+	// Hex to Int
+	i, _ := strconv.ParseInt(h, 0, 64)
+	// Unescape the string (HTML Entity -> String).
+	return html.UnescapeString(string(rune(i)))
+}
+
+func lockEmoji() string {
+	return fmt.Sprintf("%s locked", getEmoji("1F512"))
+}
+
+func unlockEmoji() string {
+	return fmt.Sprintf("%s UNLOCKED", getEmoji("1F513"))
+}
+
 func findEasyPassword(id string) string {
 	i := 0
 	j := 0
@@ -46,7 +65,7 @@ func findEasyPassword(id string) string {
 		hash := fmt.Sprintf("%x", md5.Sum([]byte(str)))
 		if strings.HasPrefix(hash, "00000") {
 			password[j] = string(hash[5])
-			fmt.Print(printPassword(1, password))
+			fmt.Printf("%s %s", printPassword(1, password), lockEmoji())
 			j++
 		}
 		if passwordComplete(password) {
@@ -72,7 +91,7 @@ func findDifficultPassword(id string) string {
 				continue
 			}
 			password[position] = string(hash[6])
-			fmt.Print(printPassword(2, password))
+			fmt.Printf("%s %s", printPassword(2, password), lockEmoji())
 		}
 		if passwordComplete(password) {
 			return composePassword(password)
@@ -82,7 +101,7 @@ func findDifficultPassword(id string) string {
 
 func main() {
 	input := file.Read()[0]
-	fmt.Print("Part 1: ________")
-	fmt.Printf("\rPart 1: %s UNLOCKED\nPart 2: ________", findEasyPassword(input))
-	fmt.Printf("\rPart 2: %s UNLOCKED\n", findDifficultPassword(input))
+	fmt.Printf("Part 1: ________ %s", lockEmoji())
+	fmt.Printf("\rPart 1: %s %s\nPart 2: ________ %s", findEasyPassword(input), unlockEmoji(), lockEmoji())
+	fmt.Printf("\rPart 2: %s %s\n", findDifficultPassword(input), unlockEmoji())
 }
