83c71cceb51fb9bf66b505ec52c9942c7381f4a0
slittlefair <sam.littlefair@sparx.co.uk>
2022-03-08 17:29:42 +0000

    Update timer comment to give implementation example (#87)

--- timer/timer.go
+++ timer/timer.go
@@ -5,7 +5,11 @@ import (
 	"time"
 )
 
-// TimeTrack prints how long a function took to run
+// TimeTrack prints how long a function took to run, printing the time since start. Initialise start
+// before the function runs and then call Track after it. e.g.
+//    t := time.Now()
+//    someFunction()
+//    timer.Track(t)
 func Track(start time.Time) {
 	elapsed := time.Since(start)
 	fmt.Printf("took %s\n", elapsed)
