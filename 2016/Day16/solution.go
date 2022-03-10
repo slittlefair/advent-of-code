807eaf5dde0167718d8165ef8bc4c92f63ed90b8
Sam Littlefair <sam.littlefair@sparx.co.uk>
2022-03-10 14:49:16 +0000

    2016/Day16

---
+++ 2016/Day16/solution.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"Advent-of-Code/file"
+	"fmt"
+)
+
+func generateDataStep(input string) (string, error) {
+	new := input + "0"
+	reverse := []byte{}
+	for i := len(input) - 1; i >= 0; i-- {
+		char := input[i]
+		switch char {
+		case '0':
+			reverse = append(reverse, '1')
+		case '1':
+			reverse = append(reverse, '0')
+		default:
+			return "", fmt.Errorf("invalid character: %b", char)
+		}
+	}
+	return new + string(reverse), nil
+}
+
+func generateData(input string, requiredLength int) (string, error) {
+	var err error
+	for len(input) < requiredLength {
+		input, err = generateDataStep(input)
+		if err != nil {
+			return "", err
+		}
+	}
+	return input[:requiredLength], nil
+}
+
+func calculateChecksum(data string) string {
+	for {
+		checksum := []byte{}
+		for i := 0; i < len(data)-1; i += 2 {
+			if data[i] == data[i+1] {
+				checksum = append(checksum, '1')
+			} else {
+				checksum = append(checksum, '0')
+			}
+		}
+		if len(checksum)%2 != 0 {
+			return string(checksum)
+		}
+		data = string(checksum)
+	}
+}
+
+func findSolution(input string, requiredDataLength int) (string, error) {
+	data, err := generateData(input, requiredDataLength)
+	if err != nil {
+		return "", err
+	}
+	return calculateChecksum(data), nil
+}
+
+func findSolutions(input string, part1Length, part2Length int) (string, string, error) {
+	part1, err := findSolution(input, part1Length)
+	if err != nil {
+		return "", "", err
+	}
+	part2, _ := findSolution(input, part2Length)
+	return part1, part2, nil
+}
+
+func main() {
+	input := file.Read()[0]
+	part1, part2, err := findSolutions(input, 272, 35651584)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Part 1:", part1)
+	fmt.Println("Part 2:", part2)
+}
---
+++ 2016/Day16/solution_test.go
@@ -0,0 +1,224 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_generateDataStep(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   string
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "returns an error if the input contains an invalid character",
+			input:   "10010010301001001110110101110101",
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name:    "returns correct data for input, advent of code example 1",
+			input:   "1",
+			want:    "100",
+			wantErr: false,
+		},
+		{
+			name:    "returns correct data for input, advent of code example 2",
+			input:   "0",
+			want:    "001",
+			wantErr: false,
+		},
+		{
+			name:    "returns correct data for input, advent of code example 3",
+			input:   "11111",
+			want:    "11111000000",
+			wantErr: false,
+		},
+		{
+			name:    "returns correct data for input, advent of code example 4",
+			input:   "111100001010",
+			want:    "1111000010100101011110000",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := generateDataStep(tt.input)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("generateDataStep() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("generateDataStep() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_generateData(t *testing.T) {
+	type args struct {
+		input          string
+		requiredLength int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "returns an error if generateDataStep returns an error",
+			args: args{
+				input:          "10010001111010100101010101010100101010101101101031010100101010100100101",
+				requiredLength: 1000,
+			},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "returns correct data for given input, advent of code example 1",
+			args: args{
+				input:          "10000",
+				requiredLength: 20,
+			},
+			want:    "10000011110010000111",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := generateData(tt.args.input, tt.args.requiredLength)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("generateData() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("generateData() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_calculateChecksum(t *testing.T) {
+	tests := []struct {
+		name string
+		data string
+		want string
+	}{
+		{
+			name: "returns correct checksum for given data, advent of code example 1",
+			data: "110010110100",
+			want: "100",
+		},
+		{
+			name: "returns correct checksum for given data, advent of code example 2",
+			data: "10000011110010000111",
+			want: "01100",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := calculateChecksum(tt.data); got != tt.want {
+				t.Errorf("calculateChecksum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_findSolution(t *testing.T) {
+	type args struct {
+		input              string
+		requiredDataLength int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "returns an error if generateData returns an error",
+			args: args{
+				input:              "1000100010001111010110010119110100010001",
+				requiredDataLength: 100,
+			},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "returns an error if generateData returns an error",
+			args: args{
+				input:              "10000",
+				requiredDataLength: 20,
+			},
+			want:    "01100",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := findSolution(tt.args.input, tt.args.requiredDataLength)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("findSolution() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("findSolution() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_findSolutions(t *testing.T) {
+	type args struct {
+		input       string
+		part1Length int
+		part2Length int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		want1   string
+		wantErr bool
+	}{
+		{
+			name: "returns an error if findSolution returns an error",
+			args: args{
+				input:       "10010011110000101101100010018101010101111100111",
+				part1Length: 100,
+				part2Length: 1000,
+			},
+			want:    "",
+			want1:   "",
+			wantErr: true,
+		},
+		{
+			name: "returns correct solutions to parts 1 and 2 (real input since AoC doesn't provide part2 example)",
+			args: args{
+				input:       "11101000110010100",
+				part1Length: 272,
+				part2Length: 35651584,
+			},
+			want:    "10100101010101101",
+			want1:   "01100001101101001",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, err := findSolutions(tt.args.input, tt.args.part1Length, tt.args.part2Length)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("findSolutions() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("findSolutions() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("findSolutions() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
