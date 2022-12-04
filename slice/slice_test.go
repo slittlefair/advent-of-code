a029ea4c5a038e54da42b2796fd1c19d7a20ebb8
slittlefair <sam.littlefair@sparx.co.uk>
2022-12-04 20:06:42 +0000

    Upgrade go and use generics for slices (#101)

--- slice/slice_test.go
+++ slice/slice_test.go
@@ -49,49 +49,6 @@ func TestPermutations(t *testing.T) {
 	}
 }
 
-func TestIntSlicesAreEqual(t *testing.T) {
-	type args struct {
-		slice1 []int
-		slice2 []int
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		{
-			name: "returns false if the given decks aren't of equal length",
-			args: args{
-				slice1: []int{1, 2, 3, 4},
-				slice2: []int{1, 2},
-			},
-			want: false,
-		},
-		{
-			name: "returns false if the given decks aren't equal",
-			args: args{
-				slice1: []int{1, 2, 3, 4, 6, 5, 7},
-				slice2: []int{1, 2, 3, 4, 5, 6, 7},
-			},
-			want: false,
-		},
-		{
-			name: "returns true if the given decks are equal",
-			args: args{
-				slice1: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-				slice2: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-			},
-			want: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := slice.IntSlicesAreEqual(tt.args.slice1, tt.args.slice2)
-			assert.Equal(t, tt.want, got)
-		})
-	}
-}
-
 func TestRemove(t *testing.T) {
 	type args struct {
 		s []int
