895b9cbed3d50ba2802b9fd352e21853739c65e8
Sam Littlefair <sam.littlefair@sparx.co.uk>
2022-12-07 20:00:38 +0000

    Refactor

--- 2022/Day07/solution.go
+++ 2022/Day07/solution.go
@@ -6,109 +6,96 @@ import (
 	"fmt"
 )
 
-type sysType int
-
-const (
-	sysFile sysType = iota
-	sysDir
-)
-
-type sys struct {
-	id       string
-	parent   *sys
-	children []*sys
-	size     int
-	sysType  sysType
+type node struct {
+	id     string
+	parent *node
+	size   int
 }
 
-type sysMap map[string]*sys
+type dirMap map[string]*node
 
-func (sm sysMap) addFileOrDir(line string, currentDir *sys) error {
+func (dm dirMap) handleFileOrDir(line string, currentDir *node) error {
 	if line[:3] == "dir" {
+		// If the line starts with "dir" then we have a directory - add this to the map along with
+		// its parent
 		var name string
 		_, err := fmt.Sscanf(line, "dir %s", &name)
 		if err != nil {
 			return fmt.Errorf("addFileOrDir on %s: %w", line, err)
 		}
-		newSys := &sys{
-			id:      fmt.Sprintf("%s/%s", currentDir.id, name),
-			parent:  currentDir,
-			sysType: sysDir,
+		newSys := &node{
+			id:     fmt.Sprintf("%s/%s", currentDir.id, name),
+			parent: currentDir,
 		}
-		currentDir.children = append(currentDir.children, newSys)
-		sm[newSys.id] = newSys
-		currentDir = newSys
+		dm[newSys.id] = newSys
 	} else {
-		var name string
+		// Otherwise we have a file, add its size to its parent, that dir's parent and so on
+		var n string
 		var size int
-		_, err := fmt.Sscanf(line, "%d %s", &size, &name)
+		_, err := fmt.Sscanf(line, "%d %s", &size, &n)
 		if err != nil {
 			return fmt.Errorf("addFileOrDir on %s: %w", line, err)
 		}
-		newSys := &sys{
-			id:      fmt.Sprintf("%s/%s", currentDir.id, name),
-			parent:  currentDir,
-			size:    size,
-			sysType: sysFile,
-		}
-		currentDir.children = append(currentDir.children, newSys)
 		currentDir.size += size
 		for currentDir.parent != nil {
 			currentDir.parent.size += size
 			currentDir = currentDir.parent
 		}
-		sm[newSys.id] = newSys
 	}
 	return nil
 }
 
-func (sm sysMap) getSub100000Dirs() int {
+func (dm dirMap) getSub100000Dirs() int {
 	sum := 0
-	for _, sys := range sm {
-		if sys.sysType == sysDir && sys.size <= 100000 {
-			sum += sys.size
+	for _, dir := range dm {
+		if dir.size <= 100000 {
+			sum += dir.size
 		}
 	}
 	return sum
 }
 
-func parseInput(input []string) (sysMap, error) {
-	currentDir := &sys{
-		id:      "/",
-		sysType: sysDir,
+func parseInput(input []string) (dirMap, error) {
+	// Set up the root node and add it to a map of all nodes
+	currentDir := &node{
+		id: "/",
 	}
-	sm := sysMap{
+	dm := dirMap{
 		"/": currentDir,
 	}
 	for i := 1; i < len(input); i++ {
 		line := input[i]
 		if string(line[0]) != "$" {
-			err := sm.addFileOrDir(line, currentDir)
+			// If the line is not a command, it must be listing a directory or file
+			err := dm.handleFileOrDir(line, currentDir)
 			if err != nil {
 				return nil, err
 			}
 		} else if line == "$ cd .." {
+			// Move up to the parent directory
 			currentDir = currentDir.parent
 		} else if line == "$ ls" {
+			// Don't do anything, we handle following list in the above
 			continue
 		} else {
+			// If none of the above we must be going into one of the child directories
 			var dir string
 			_, err := fmt.Sscanf(line, "$ cd %s", &dir)
 			if err != nil {
 				return nil, fmt.Errorf("parseInput on %s: %w", line, err)
 			}
-			currentDir = sm[fmt.Sprintf("%s/%s", currentDir.id, dir)]
+			currentDir = dm[fmt.Sprintf("%s/%s", currentDir.id, dir)]
 		}
 	}
-	return sm, nil
+	return dm, nil
 }
 
-func (sm sysMap) findDirToDelete() int {
-	minSpaceToDelete := 30000000 - (70000000 - sm["/"].size)
+func (dm dirMap) findDirToDelete() int {
+	minSpaceToDelete := 30000000 - (70000000 - dm["/"].size)
 	sizeToDelete := maths.Infinity
-	for _, sys := range sm {
-		if sys.sysType == sysDir && sys.size >= minSpaceToDelete && sys.size < sizeToDelete {
-			sizeToDelete = sys.size
+	for _, dir := range dm {
+		if dir.size >= minSpaceToDelete && dir.size < sizeToDelete {
+			sizeToDelete = dir.size
 		}
 	}
 	return sizeToDelete
@@ -116,11 +103,11 @@ func (sm sysMap) findDirToDelete() int {
 
 func main() {
 	input := file.Read()
-	sm, err := parseInput(input)
+	dm, err := parseInput(input)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println("Part 1:", sm.getSub100000Dirs())
-	fmt.Println("Part 2:", sm.findDirToDelete())
+	fmt.Println("Part 1:", dm.getSub100000Dirs())
+	fmt.Println("Part 2:", dm.findDirToDelete())
 }
