c33050ce47154b0f6b307b88a6963a401230f328
Sam Littlefair <sam.littlefair@sparx.co.uk>
2022-12-08 17:02:07 +0000

    Refactor

--- 2022/Day08/solution.go
+++ 2022/Day08/solution.go
@@ -26,107 +26,96 @@ func parseInput(input []string) TreeMap {
 	return tm
 }
 
-// func (tm TreeMap) traverseDirection() {
+type Direction int
 
-// }
+const (
+	Horizontally Direction = iota
+	Vertically
+)
 
-func (tm TreeMap) countVisibleTrees() (int, int) {
+// Move from a particular coordinate to a particular coordinate and calculate whether it's visible
+// and what the viewing distance is
+func (tm TreeMap) travel(co graph.Co, from, to, change int, direction Direction) (bool, int) {
+	dist := 0
+	viewingDistance := 0
+	visible := true
+	for i := from; i != to; i = i + change {
+		dist++
+		x := co.X
+		y := co.Y
+		if direction == Horizontally {
+			x = i
+		} else {
+			y = i
+		}
+		if tm.graph[graph.Co{X: x, Y: y}] >= tm.graph[co] {
+			visible = false
+			if viewingDistance == 0 {
+				viewingDistance = dist
+			}
+		}
+	}
+	if viewingDistance == 0 {
+		viewingDistance = dist
+	}
+	return visible, viewingDistance
+}
+
+func (tm TreeMap) optimiseTreehouseLocation() (int, int) {
 	sum := 0
 	highestScenicScore := 0
-	for co, v := range tm.graph {
+	for co := range tm.graph {
 		// if a tree is on the edge then it is visible
 		if co.X == 0 || co.X == tm.maxX || co.Y == 0 || co.Y == tm.maxY {
 			sum++
 		} else {
-			visibleDirections := [4]bool{true, true, true, true}
+			var visible bool
+			var viewingDistance int
+			visibleDirections := [4]bool{}
 			scenicScore := 1
 
 			// Go left
-			dist := 0
-			blockedAt := 0
-			for x := co.X - 1; x >= 0; x-- {
-				dist++
-				if tm.graph[graph.Co{X: x, Y: co.Y}] >= v {
-					visibleDirections[0] = false
-					if blockedAt == 0 {
-						blockedAt = dist
-					}
-				}
-			}
-			if blockedAt == 0 {
-				blockedAt = dist
-			}
-			scenicScore *= blockedAt
+			visible, viewingDistance = tm.travel(co, co.X-1, -1, -1, Horizontally)
+			visibleDirections[0] = visible
+			scenicScore *= viewingDistance
 
 			// Go right
-			dist = 0
-			blockedAt = 0
-			for x := co.X + 1; x <= tm.maxX; x++ {
-				dist++
-				if tm.graph[graph.Co{X: x, Y: co.Y}] >= v {
-					visibleDirections[1] = false
-					if blockedAt == 0 {
-						blockedAt = dist
-					}
-				}
-			}
-			if blockedAt == 0 {
-				blockedAt = dist
-			}
-			scenicScore *= blockedAt
+			visible, viewingDistance = tm.travel(co, co.X+1, tm.maxX+1, 1, Horizontally)
+			visibleDirections[1] = visible
+			scenicScore *= viewingDistance
 
 			// Go up
-			dist = 0
-			blockedAt = 0
-			for y := co.Y - 1; y >= 0; y-- {
-				dist++
-				if tm.graph[graph.Co{X: co.X, Y: y}] >= v {
-					visibleDirections[2] = false
-					if blockedAt == 0 {
-						blockedAt = dist
-					}
-				}
-			}
-			if blockedAt == 0 {
-				blockedAt = dist
-			}
-			scenicScore *= blockedAt
+			visible, viewingDistance = tm.travel(co, co.Y-1, -1, -1, Vertically)
+			visibleDirections[2] = visible
+			scenicScore *= viewingDistance
 
 			// Go down
-			dist = 0
-			blockedAt = 0
-			for y := co.Y + 1; y <= tm.maxY; y++ {
-				dist++
-				if tm.graph[graph.Co{X: co.X, Y: y}] >= v {
-					visibleDirections[3] = false
-					if blockedAt == 0 {
-						blockedAt = dist
-					}
-				}
-			}
-			if blockedAt == 0 {
-				blockedAt = dist
-			}
-			scenicScore *= blockedAt
+			visible, viewingDistance = tm.travel(co, co.Y+1, tm.maxY+1, 1, Vertically)
+			visibleDirections[3] = visible
+			scenicScore *= viewingDistance
 
+			// check all directions and if any of them are visible then the tree is visible
 			for _, b := range visibleDirections {
 				if b {
 					sum++
 					break
 				}
 			}
+
+			// compare the tree's scenic score with the current highest and set it if it's higher
 			if scenicScore > highestScenicScore {
 				highestScenicScore = scenicScore
 			}
 		}
 	}
+
 	return sum, highestScenicScore
 }
 
 func main() {
 	input := file.Read()
 	tm := parseInput(input)
-	part1, part2 := tm.countVisibleTrees()
+	part1, part2 := tm.optimiseTreehouseLocation()
 	fmt.Println("Part 1:", part1)
 	fmt.Println("Part 2:", part2)
 }
