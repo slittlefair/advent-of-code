b1656f6682233edc626ca1571854006244159102
Sam Littlefair <sam.littlefair@sparx.co.uk>
2024-12-17 13:01:21 +0000

    Day4 solution

--- 2024/README.md
+++ 2024/README.md
@@ -1,3 +1,4 @@
 # :christmas_tree: 2024 :christmas_tree:
 
 - [x] Day 1 - Historian Hysteria :nerd_face: :scream:
+- [x] Day 4 - Ceres Search :full_moon: :mag:
--- graph/graph.go
+++ graph/graph.go
@@ -1,12 +1,36 @@
 package graph
 
-import "Advent-of-Code/maths"
+import (
+	"Advent-of-Code/maths"
+	"fmt"
+)
 
 // Co is a simple struct for a graph coordinate with points x, y, z
 type Co struct {
 	X int
 	Y int
 	Z int
+}
+
+// Grid is a struct containing min and max values as well as a map of coordinates to string values
+type Grid struct {
+	Graph                  map[Co]string
+	MinX, MaxX, MinY, MaxY int
+}
+
+func NewGrid() *Grid {
+	return &Grid{
+		Graph: make(map[Co]string),
+	}
+}
+
+func (g *Grid) PrintGrid() {
+	for y := 0; y <= g.MaxY; y++ {
+		for x := 0; x <= g.MaxX; x++ {
+			fmt.Print(g.Graph[Co{X: x, Y: y}])
+		}
+		fmt.Println()
+	}
 }
 
 // AdjacentCos returns all adjacent coordinates for the given coordinate, including diagonals
