076d1f438b923abad6663d40af719ff6e82fde7b
slittlefair <sam.littlefair@sparx.co.uk>
2024-12-19 13:17:18 +0000

    2024/day03 (#137)

---
+++ 2024/Day03/solution.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"Advent-of-Code/file"
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+var reMulPart1 = regexp.MustCompile(`mul\(\d+,\d+\)`)
+var reMulPart2 = regexp.MustCompile(`mul\(\d+,\d+\)|do\(\)|don't\(\)`)
+var reNum = regexp.MustCompile(`\d+`)
+
+func getMultiplication(mul string) int {
+	// mul is already regex matched to ensure we have two numbers, so we don't need to validate
+	// or handle any errors
+	nums := reNum.FindAllString(mul, 2)
+	n1, _ := strconv.Atoi(nums[0])
+	n2, _ := strconv.Atoi(nums[1])
+	return n1 * n2
+}
+
+func findSolutions(input []string) (int, int) {
+	var part1, part2 int
+	mulEnabled := true
+	for _, line := range input {
+		matches := reMulPart1.FindAllString(line, -1)
+		for _, mul := range matches {
+			part1 += getMultiplication(mul)
+		}
+
+		matches = reMulPart2.FindAllString(line, -1)
+		for _, m := range matches {
+			if m == "do()" {
+				mulEnabled = true
+				continue
+			}
+			if m == "don't()" {
+				mulEnabled = false
+				continue
+			}
+			if !mulEnabled {
+				continue
+			}
+			part2 += getMultiplication(m)
+		}
+	}
+	return part1, part2
+}
+
+func main() {
+	input := file.Read()
+	part1, part2 := findSolutions(input)
+	fmt.Printf("Part1: %d\n", part1)
+	fmt.Printf("Part2: %d\n", part2)
+}
--- 2024/README.md
+++ 2024/README.md
@@ -2,4 +2,5 @@
 
 - [x] Day 1 - Historian Hysteria :nerd_face: :scream:
 - [x] Day 2 - Red-Nosed Reports :small_red_triangle: :page_facing_up:
+- [x] Day 3 - Mull It Over :wine_glass: :arrow_upper_right:s
 - [x] Day 4 - Ceres Search :full_moon: :mag:
