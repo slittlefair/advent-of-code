cbb8a7ad746bf7dd19469393e3cac9eef80a03ce
Sam Littlefair <sam.littlefair@sparx.co.uk>
2025-01-06 09:38:52 +0000

    Improved timer

--- timer/timer.go
+++ timer/timer.go
@@ -2,15 +2,21 @@ package timer
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
 // TimeTrack prints how long a function took to run, printing the time since start. Initialise start
 // before the function runs and then call Track after it. e.g.
-//    t := time.Now()
-//    someFunction()
-//    timer.Track(t)
-func Track(start time.Time) {
-	elapsed := time.Since(start)
-	fmt.Printf("took %s\n", elapsed)
+//
+//	t := time.Now()
+//	someFunction()
+//	timer.Track(t)
+func Track(start time.Time, messages ...string) {
+	elapsed := fmt.Sprintf("took %s\n", time.Since(start))
+	if len(messages) == 0 {
+		fmt.Print(elapsed)
+		return
+	}
+	fmt.Printf("%s: %s", strings.Join(messages, ": "), elapsed)
 }
